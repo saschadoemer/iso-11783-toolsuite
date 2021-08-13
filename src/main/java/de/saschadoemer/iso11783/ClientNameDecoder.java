093d94515652360ef091ee5f79cfd2f547db8b01
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2021-08-13 22:33:32 +0200

    Check length of the client name.

--- src/main/java/de/saschadoemer/iso11783/ClientNameDecoder.java
+++ src/main/java/de/saschadoemer/iso11783/ClientNameDecoder.java
@@ -1,9 +1,13 @@
 package de.saschadoemer.iso11783;
 
+import java.util.logging.Logger;
+
 /**
  * Client name decoder for ISO 11783-5 encoded client names.
  */
 public final class ClientNameDecoder {
+
+    private static final Logger LOG = Logger.getLogger(ClientNameDecoder.class.getName());
 
     /**
      * Hide the constructor.
@@ -18,7 +22,17 @@ public final class ClientNameDecoder {
      */
     public static void decode(byte[] clientName) {
         if (null == clientName || clientName.length == 0) {
+            LOG.warning("Could not decode client name, since the client name was null or empty.");
             throw new IllegalArgumentException("The client name can not be null or empty");
+        }
+        validateClientName(clientName);
+    }
+
+    private static void validateClientName(byte[] clientName) {
+        final String hexRepresentation = new String(clientName);
+        if (hexRepresentation.length() != 16) {
+            LOG.warning("The client name has the wrong length. The client name has to have exactly 16 digits.");
+            throw new IllegalArgumentException(String.format("Wrong client name format, expected 15 digits,found %d digits.", hexRepresentation.length()));
         }
     }
 
--- src/test/java/de/saschadoemer/iso11783/ClientNameDecoderTest.java
+++ src/test/java/de/saschadoemer/iso11783/ClientNameDecoderTest.java
@@ -2,6 +2,8 @@ package de.saschadoemer.iso11783;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ClientNameDecoderTest {
@@ -12,9 +14,19 @@ class ClientNameDecoderTest {
     }
 
     @Test
-    @SuppressWarnings("ObviousNullCheck")
     void givenEmptyValueWhenDecodingTheClientNameThenTheDecoderShouldThrowAnException() {
         assertThrows(IllegalArgumentException.class, () -> ClientNameDecoder.decode(new byte[0]));
+    }
+
+    @Test
+    void givenInvalidHexValueWhenDecodingTheClientNameThenTheDecoderShouldDecodeAllFieldsCorrectly() {
+        assertThrows(IllegalArgumentException.class, () -> ClientNameDecoder.decode("A01284000DE0C3F".getBytes(StandardCharsets.UTF_8))); // Less digits
+        assertThrows(IllegalArgumentException.class, () -> ClientNameDecoder.decode("A01284000DE0C3FFF".getBytes(StandardCharsets.UTF_8))); // Too much digits
+    }
+
+    @Test
+    void givenValidHexValueWhenDecodingTheClientNameThenTheDecoderShouldDecodeAllFieldsCorrectly() {
+        final String hexValue = "A01284000DE0C3FF";
     }
 
 }
